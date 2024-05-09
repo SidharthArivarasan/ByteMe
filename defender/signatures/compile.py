7ac6db246bcd387c08f31bd85c3abbc21d04b1a3
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2024-05-08 19:06:40 -0500

    Add signature compilation step to Dockerfile

---
+++ defender/signatures/compile.py
@@ -0,0 +1,6 @@
+from .main import compile_and_save_rules
+
+
+compile_and_save_rules("signatures/malware")
+compile_and_save_rules("signatures/packer_crypto")
+compile_and_save_rules("signatures/yaraGen")
