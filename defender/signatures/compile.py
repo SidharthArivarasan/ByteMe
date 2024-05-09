571176bb507ee083df080894ee750a1361a8f957
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2024-05-08 19:38:46 -0500

    Update file paths in compile.py

--- defender/signatures/compile.py
+++ defender/signatures/compile.py
@@ -1,6 +1,9 @@
+import os
+
 from main import compile_and_save_rules
 
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-compile_and_save_rules("./rules/malware")
-compile_and_save_rules("./rules/packer_crypto")
-compile_and_save_rules("./rules/yaraGen")
+compile_and_save_rules(os.path.join(ROOT_DIR, "rules", "malware"))
+compile_and_save_rules(os.path.join(ROOT_DIR, "rules", "packer_crypto"))
+compile_and_save_rules(os.path.join(ROOT_DIR, "rules", "yaraGen"))
