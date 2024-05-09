9832c573058cc48b628f258b28d85f1c5b321642
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2024-05-08 19:10:40 -0500

    Update file paths in compile.py

--- defender/signatures/compile.py
+++ defender/signatures/compile.py
@@ -1,6 +1,6 @@
 from main import compile_and_save_rules
 
 
-compile_and_save_rules("signatures/malware")
-compile_and_save_rules("signatures/packer_crypto")
-compile_and_save_rules("signatures/yaraGen")
+compile_and_save_rules("./rules/malware")
+compile_and_save_rules("./rules/packer_crypto")
+compile_and_save_rules("./rules/yaraGen")
